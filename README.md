ef5ee2d4334139617e076ec208ee6467f02f0e47
Amanda Lee <leeandama87@gmail.com>
2022-11-29 15:14:10 -0800

    Added to README file and API function to script.js file.

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Weather-Dashboard
 
 ## Description
+- This project was to create a weather dashboard where the user can search for the weather in a US city of their request. 
+- This is possible due to the weather API used for the project.
+- A user can type in any city in the US and the webpage will return the forecast for the day and for 5 days out. It will include results of the temperature, wind speed, and humidity.
 
-## Screenshot
+## Mockup
+![Mockup](assets/images/demo.png)
+
+## Screenshot of Webpage
+
 
 ## Deployed Application
+Weather Dashboard [Application]()!
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+var searchBtn = document.getElementById("search-btn")
+var inputCity = document.getElementById("input-city")
+
 function getApi() {
-    var requestUrl = '';
-}
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q='+inputCity.value+'&appid=a076f8aca8be2745eedd972c6da85de1";
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (err) {
+        alert("Not a city!");
+    })
+};
+
+searchBtn.addEventListener('click', getApi);
